7382786646e36ece4971dfa7d70d2b25a7bcb79a
Jeet Roy <jeetroy251@gmail.com>
2026-07-04 01:50:55 +0530

    changes done for deploy

--- dpi-dashboard/src/App.jsx
+++ dpi-dashboard/src/App.jsx
@@ -12,7 +12,7 @@ import {
   YAxis
 } from 'recharts';
 
-const API_BASE = 'http://localhost:3000';
+const API_BASE = import.meta.env.VITE_API_URL;
 
 function parseArrayField(value) {
   if (!value) return [];
